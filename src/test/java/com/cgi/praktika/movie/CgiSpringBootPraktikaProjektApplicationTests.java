a2bd91ec1fdc21b653f6f170dd2e3b5b7916ff79
olegtrofimov <olegtrofimov96@gmail.com>
2024-03-04 11:03:40 +0200

    Initial commit

---
+++ src/test/java/com/cgi/praktika/movie/CgiSpringBootPraktikaProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.praktika.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgiSpringBootPraktikaProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
